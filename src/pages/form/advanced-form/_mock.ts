c21736479ec1a94b6f873c2a93bbc515390755a9
sakura-flutter <28331128+sakura-flutter@users.noreply.github.com>
2022-06-28 17:02:18 +0800

    feat: init

---
+++ src/pages/form/advanced-form/_mock.ts
@@ -0,0 +1,8 @@
+//
+import type { Request, Response } from 'express';
+
+export default {
+  'POST  /api/advancedForm': (_: Request, res: Response) => {
+    res.send({ data: { message: 'Ok' } });
+  },
+};
